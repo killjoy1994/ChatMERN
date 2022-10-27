d46057cdae22b99dfa3930519cd740a005d40dea
Bagus Nugroho <papaobobo@gmail.com>
2022-10-27 16:07:05 +0700

    socket io connection setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const htpp = require("http");
+const { Server } = require("socket.io");
+
+//socket io connection setup
 const app = express();
+const httpServer = htpp.createServer(app);
+const io = new Server(httpServer);
+
+// io.on("connection", (socket) => {
+//   // ...
+// });
 
 app.get("/", (req, res) => res.json({ data: "hello world from socket" }));
 
